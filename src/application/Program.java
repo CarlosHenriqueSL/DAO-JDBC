e6911a0cb20d534e81ba5a8242241f46d1841808
CarlosHenriqueSL <luiscarloslopes597@gmail.com>
2025-11-19 15:01:20 -0300

    SellerDAOJDBC and DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,9 @@ public class Program {
         Department department = new Department(1, "books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.00, department);
+
+        SellerDAO sellerDAO = DAOFactory.createSellerDAO();
+        
         System.out.println(seller);
     }
 }
---
+++ src/model/dao/impl/SellerDAOJDBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDAO;
+import model.entities.Seller;
+
+public class SellerDAOJDBC implements SellerDAO {
+
+    @Override
+    public void insert(Seller seller) {
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void update(Seller seller) {
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+}
